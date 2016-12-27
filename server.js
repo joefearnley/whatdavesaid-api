2aeae5a0d75ba80bec005f418e4dae8e2b9744e1
Joe Fearnley <joe.fearnley@gmail.com>
2016-12-27 12:20:05 -0500

    My wife is shopping, zig and I are watching the wizard of Oz, and I have some grand plans

--- server.js
+++ server.js
@@ -12,10 +12,26 @@ server.start((err) => {
     console.log(`Server running at: ${server.info.uri}`);
 });
 
+/**
+ *  Get all clips.
+ *
+ * @type String (JSON? I'm building an API here, man)
+ */
 server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
+        // return all the clips.....
         reply('Hello, world!');
     }
+});
+
+/**
+ * Get a random clip.
+ *
+ * @type String (JSON? I'm building an API here, man)
+ */
+server.route({
+    method: 'GET',
+    path:: '/random'
 });
