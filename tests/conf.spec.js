a7bdb0bfa8ca8e0267cc637d4c89571119269a92
Joe Fearnley <joe.fearnley@gmail.com>
2016-12-28 21:30:59 -0500

    Setting up jasmine core for testing;

---
+++ tests/conf.spec.js
@@ -0,0 +1,3 @@
+var SpecReporter = require('jasmine-spec-reporter');
+var reporter = new SpecReporter();
+jasmine.getEnv().addReporter(reporter);
--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,3 +1,37 @@
+const request = require('request')
+const nock = require('nock');
+const baseUrl = 'http://localhost:5000/';
+const fakeResponse = [{
+    'name': 'black-metal',
+    'title': 'Black Metal',
+    'url': 'black-metal.mp3'
+},{
+    'name': 'hot-damn',
+    'title': 'Hot Damn',
+    'url': 'hot-damn.mp3'
+}];
+
 describe('Clips', function() {
-    it('should list all clips');
+
+    describe('GET /', function() {
+        nock(baseUrl).get('/').reply(200, fakeResponse);
+
+        it('should list all clips', function() {
+            request.get(url, function(error, response, body) {
+                expect(response).toBeDefined();
+
+                
+            });
+        });
+    });
+
+    describe('GET /random', function() {
+        nock(baseUrl).get('/').reply(200, fakeResponse);
+
+        it('should list a random clip', function() {
+            request.get(url, function(error, response, body) {
+                expect(response).toBeDefined();
+            });
+        });
+    });
 });
